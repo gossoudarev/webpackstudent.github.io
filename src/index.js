0482227fddbee22092debe9c92f54a9fb01a7660
webpackstudent <43551569+webpackstudent@users.noreply.github.com>
2018-09-25 01:08:44 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Component as C } from 'react';
+import { render as r } from 'react-dom';
+
+import Degs from './components/Degs';
+
+class App extends C {
+  constructor() {
+    super();
+    this.state = {
+      exps: 5
+    }
+  }
+  click() {
+    this.setState(({ exps }) => ({ exps: +exps + 1 }));
+  }
+  change(v) {
+    this.setState(({ exps }) => ({ exps: v }))
+  }
+  render(){
+    return (<div>
+      <button onClick={this.click.bind(this)}>⬆︎</button>
+      <input value={ this.state.exps }
+             onChange={({ target: {value: v}}) => this.change(v)} />
+      <Degs a={ Array.from({ length: this.state.exps }, (v, i) => 10 + i) } />
+    </div>)
+  }
+}
+
+r(<App />,
+document.querySelector('.cont'));
